5a58c01893e9c84b8476c3824d8256c0d3dbb3d1
Niki2222 <89404731+Niki2222@users.noreply.github.com>
2024-01-30 10:14:38 +0200

    reset button working

--- script.js
+++ script.js
@@ -3,16 +3,10 @@ let mark;
 let count = 0;
 let winX = 0;
 let win0 = 0;
+let gameInProgress = true;
 
-function startPlayerX() {
-    document.getElementById('btnX');
-    mark = "X";
-    playGame();
-}
-
-function startPlayerY() {
-    document.getElementById('btn0');
-    mark = "0";
+function startPlayer(marks) {
+    mark = marks;
     playGame();
 }
 
@@ -21,72 +15,72 @@ function playGame() {
     document.getElementById('btn0').disabled = true;
     for (let i = 0; i < divs.length; ++i) {
         count = 0;
-        divs[i].addEventListener("click", function() {
-            this.innerText = mark;
-            if (mark === "X") {
-                mark = "0";
-            } else if (mark === "0") {
-                mark = "X";
-            }
-            ++count;
-            console.log(count, "first time play");
-
-            if ((divs[0].innerText === "X" && (divs[0].innerText === divs[1].innerText 
-                && divs[1].innerText === divs[2].innerText 
-                || divs[0].innerText === divs[3].innerText 
-                && divs[3].innerText === divs[6].innerText
-                || divs[0].innerText === divs[4].innerText  
-                && divs[4].innerText === divs[8].innerText))
-                || (divs[1].innerText === "X" && divs[1].innerText === divs[4].innerText 
-                && divs[4].innerText === divs[7].innerText)
-                || (divs[2].innerText === "X" && (divs[2].innerText === divs[4].innerText 
-                && divs[4].innerText === divs[6].innerText
-                || divs[2].innerText === divs[5].innerText 
-                && divs[5].innerText === divs[8].innerText))
-                || (divs[3].innerText === "X" && divs[3].innerText === divs[4].innerText 
-                && divs[4].innerText === divs[5].innerText)
-                || (divs[6].innerText === "X" && divs[6].innerText === divs[7].innerText 
-                && divs[7].innerText === divs[8].innerText)) {
-                winX = 1;
-            } 
-            if ((divs[0].innerText === "0" && (divs[0].innerText === divs[1].innerText 
-                && divs[1].innerText === divs[2].innerText 
-                || divs[0].innerText === divs[3].innerText 
-                && divs[3].innerText === divs[6].innerText
-                || divs[0].innerText === divs[4].innerText 
-                && divs[4].innerText === divs[8].innerText))
-                || (divs[1].innerText === "0" && divs[1].innerText === divs[4].innerText 
-                && divs[4].innerText === divs[7].innerText)
-                || (divs[2].innerText === "0" && (divs[2].innerText === divs[4].innerText 
-                && divs[4].innerText === divs[6].innerText
-                || divs[2].innerText === divs[5].innerText 
-                && divs[5].innerText === divs[8].innerText))
-                || (divs[3].innerText === "0" && divs[3].innerText === divs[4].innerText 
-                && divs[4].innerText === divs[5].innerText)
-                || (divs[6].innerText === "0" && divs[6].innerText === divs[7].innerText 
-                && divs[7].innerText === divs[8].innerText)) {
-                win0 = 1;
-            } 
-            if (winX === 1) {
-                document.getElementById('winner').innerHTML = `<h2>PlayerX WON!!!</h2>`;
-            } else if (win0 === 1) {
-                document.getElementById('winner').innerHTML = `<h2>Player0 WON!!!</h2>`;
-            } else if (count === 9) {
-                document.getElementById('winner').innerHTML = `<h2>There is no winner...</h2>`;
-            }
-        });
+        if (divs[i].innerText === "" && gameInProgress) {
+            divs[i].addEventListener("click", function() {
+                this.innerText = mark;
+                if (mark === "X") {
+                    mark = "0";
+                } else if (mark === "0") {
+                    mark = "X";
+                }
+                ++count;
+                if ((divs[0].innerText === "X" && (divs[0].innerText === divs[1].innerText 
+                    && divs[1].innerText === divs[2].innerText 
+                    || divs[0].innerText === divs[3].innerText 
+                    && divs[3].innerText === divs[6].innerText
+                    || divs[0].innerText === divs[4].innerText  
+                    && divs[4].innerText === divs[8].innerText))
+                    || (divs[1].innerText === "X" && divs[1].innerText === divs[4].innerText 
+                    && divs[4].innerText === divs[7].innerText)
+                    || (divs[2].innerText === "X" && (divs[2].innerText === divs[4].innerText 
+                    && divs[4].innerText === divs[6].innerText
+                    || divs[2].innerText === divs[5].innerText 
+                    && divs[5].innerText === divs[8].innerText))
+                    || (divs[3].innerText === "X" && divs[3].innerText === divs[4].innerText 
+                    && divs[4].innerText === divs[5].innerText)
+                    || (divs[6].innerText === "X" && divs[6].innerText === divs[7].innerText 
+                    && divs[7].innerText === divs[8].innerText)) {
+                    winX = 1;
+                } 
+                if ((divs[0].innerText === "0" && (divs[0].innerText === divs[1].innerText 
+                    && divs[1].innerText === divs[2].innerText 
+                    || divs[0].innerText === divs[3].innerText 
+                    && divs[3].innerText === divs[6].innerText
+                    || divs[0].innerText === divs[4].innerText 
+                    && divs[4].innerText === divs[8].innerText))
+                    || (divs[1].innerText === "0" && divs[1].innerText === divs[4].innerText 
+                    && divs[4].innerText === divs[7].innerText)
+                    || (divs[2].innerText === "0" && (divs[2].innerText === divs[4].innerText 
+                    && divs[4].innerText === divs[6].innerText
+                    || divs[2].innerText === divs[5].innerText 
+                    && divs[5].innerText === divs[8].innerText))
+                    || (divs[3].innerText === "0" && divs[3].innerText === divs[4].innerText 
+                    && divs[4].innerText === divs[5].innerText)
+                    || (divs[6].innerText === "0" && divs[6].innerText === divs[7].innerText 
+                    && divs[7].innerText === divs[8].innerText)) {
+                    win0 = 1;
+                } 
+                if (winX === 1) {
+                    document.getElementById('winner').innerHTML = `<h2>PlayerX WON!!!</h2>`;
+                } else if (win0 === 1) {
+                    document.getElementById('winner').innerHTML = `<h2>Player0 WON!!!</h2>`;
+                } else if (count === 9) {
+                    document.getElementById('winner').innerHTML = `<h2>There is no winner...</h2>`;
+                }
+                if (winX === 1 || win0 === 1 || count === 9) {
+                    gameInProgress = false;
+                }
+            });
+        }
     }
-    if (winX === 1 || win0 === 1 || count === 9) {
-        return;
-    } 
 }
-    
+
 function resetAll() {
     divs.forEach(element => {
         element.innerText = "";
     });
     document.getElementById('winner').innerHTML = "";
-    mark = "a";
+    mark = "";
     count = 0;
     winX = 0;
     win0 = 0;
