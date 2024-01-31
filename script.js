d79983135c838bcc477f9656515a18e7c7931a3d
Niki2222 <89404731+Niki2222@users.noreply.github.com>
2024-01-31 14:06:35 +0200

    used array for checkConditions update

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let divs = document.querySelectorAll('.col');
 let mark;
 let count = 0;
-let winX = 0;
-let win0 = 0;
+let winXOr0 = "";
+let winningCombitations = [
+    [0, 1, 2], [0, 3, 6], [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6], [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+];
 
 function startPlayer(marks) {
     mark = marks;
@@ -29,42 +35,31 @@ function playGame() {
 }
 
 function checkConditions(elem) {
-    if ((divs[0].innerText === elem && (divs[0].innerText === divs[1].innerText 
-        && divs[1].innerText === divs[2].innerText 
-        || divs[0].innerText === divs[3].innerText 
-        && divs[3].innerText === divs[6].innerText
-        || divs[0].innerText === divs[4].innerText  
-        && divs[4].innerText === divs[8].innerText))
-        || (divs[1].innerText === elem && divs[1].innerText === divs[4].innerText 
-        && divs[4].innerText === divs[7].innerText)
-        || (divs[2].innerText === elem && (divs[2].innerText === divs[4].innerText 
-        && divs[4].innerText === divs[6].innerText
-        || divs[2].innerText === divs[5].innerText 
-        && divs[5].innerText === divs[8].innerText))
-        || (divs[3].innerText === elem && divs[3].innerText === divs[4].innerText 
-        && divs[4].innerText === divs[5].innerText)
-        || (divs[6].innerText === elem && divs[6].innerText === divs[7].innerText 
-        && divs[7].innerText === divs[8].innerText)) {
-        if (elem === "X") {
-            winX = 1;
-        } else if (elem === "0") {
-            win0 = 1;
-        }  
-    }  
+    for (let i = 0; i < winningCombitations.length; ++i) {
+        let [a, b, c] = winningCombitations[i];
+        if (divs[a].innerText === elem && divs[a].innerText === divs[b].innerText
+            && divs[b].innerText === divs[c].innerText) {
+            if (elem === "X") {
+                winXOr0 = "X";
+            } else if ( elem === "0") {
+                winXOr0 = "0";
+            }
+        }
+    }
 }
 
 function findWinner() {
     for (let i = 0; i < divs.length; ++i) {
         checkConditions(mark);
-        if (winX === 1) {
+        if (winXOr0 === "X") {
             document.getElementById('winner').innerHTML = `<h2>PlayerX WON!!!</h2>`;
-        } else if (win0 === 1) {
+        } else if (winXOr0 === "0") {
             document.getElementById('winner').innerHTML = `<h2>Player0 WON!!!</h2>`;
         } else if (count === 9) {
             document.getElementById('winner').innerHTML = `<h2>There is no winner...</h2>`;
         }
     }
-    if (winX === 1 || win0 === 1 || count === 9) {
+    if (winXOr0 || count === 9) {
         removeEvents();
     }
 }
